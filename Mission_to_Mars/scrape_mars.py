0c335d20ce70119710f4f6a39276ad3a003c3d33
Lindsey Morales <lindseyvmorales@gmail.com>
2022-05-31 10:49:25 -0400

    Updated app and scrape

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -8,6 +8,8 @@ import time
 
 
 def scrape():
+
+    mars_data = {}
     
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
@@ -36,10 +38,16 @@ def scrape():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
     mars_table_webpage = browser.visit(mars_table_url)
+    time.sleep(3)
     html = browser.html
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser') 
     table_html = soup.find('table', class_='table table-striped')
-    print(table_html)
+    table = pd.read_html("https://galaxyfacts-mars.com/")
+    df = table[0]
+    mars_table = df.rename(columns={0: 'Mars-Earth Comparison', 1:'Mars', 2: 'Earth'})
+    mars_table.set_index('Mars-Earth Comparison')
+    mars_table = mars_table.to_html(classes='table table-stripped', justify='left', border=1)
+    mars_table = mars_table.replace('\n', '')
     browser.quit()
 
     astrology_url = "https://marshemispheres.com/"
@@ -54,17 +62,15 @@ def scrape():
     titles = []
     links = []
 
-    for h3 in range(0,4):
+    for h3 in range(0, len(anchor)):
         descriptions = anchor[h3].text
         titles.append(descriptions)
 
     for title in titles:
         browser.links.find_by_partial_text(title).click()
         browser.links.find_by_text('Open').click()
-        
         html = browser.html
         soup = BeautifulSoup(html, 'html.parser')
-
         image_link = astrology_url + soup.find('img', class_="wide-image")['src']
         title_img_dict = {}
         title_img_dict['title'] = title
@@ -77,12 +83,12 @@ def scrape():
     print(links)
     browser.quit()
 
-    mars_data = {}
+    
     mars_data['news_title'] = news_title
     mars_data['news_p'] = news_p
     mars_data['img_complete_url'] = img_complete_url
-    mars_data['table_html'] = table_html
-    # mars_data['links'] = links
+    mars_data['mars_table'] = mars_table
+    mars_data['links'] = links
 
     return mars_data
 
